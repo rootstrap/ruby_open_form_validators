1a644b3f557c17a34f15f674686aa33f292d8def
Elián Marchisio <elian.marchisio@rootstrap.com>
2019-08-09 14:37:53 -0300

    Use a more accurate email regexp

--- lib/ruby_open_form_validators/constants.rb
+++ lib/ruby_open_form_validators/constants.rb
@@ -1,6 +1,10 @@
 module RubyOpenFormValidators
   module Constants
-    EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.freeze
+    EMAIL_REGEX = /\A[a-zA-Z0-9][a-zA-Z0-9.!\#$%&'*+\/=?^_`{|}~-]*
+                  @
+                  [a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?
+                  (?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*\z/x.freeze
+
     DATE_FORMAT = '%Y%m%d'.freeze
   end
 end
